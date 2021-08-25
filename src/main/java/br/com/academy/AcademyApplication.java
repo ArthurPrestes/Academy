31be87fc36552e254f814e929b9fda4a98f8767c
ArthurPrestes <66209226+ArthurPrestes@users.noreply.github.com>
2021-08-25 00:53:11 -0300

    atualizalção e formatação de algumas classes, folder aluno e implementação das classes Formaluno e AlunoControler

--- src/main/java/br/com/academy/AcademyApplication.java
+++ src/main/java/br/com/academy/AcademyApplication.java
@@ -2,9 +2,11 @@ package br.com.academy;
 
 import org.springframework.boot.SpringApplication;
 import javax.persistence.Entity;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Entity
 @SpringBootApplication
+
 public class AcademyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -3,17 +3,16 @@ package br.com.academy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import br.com.academy.model.Aluno;
 
 @Controller
-public class HomeController {
-	
-	@GetMapping("/")
-	public ModelAndView index() {
+public class AlunoController {
+
+	@GetMapping("/InserirAlunos")
+	public ModelAndView insertAlunos(Aluno aluno) {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("home/index");
-		mv.addObject("msg","Mensagem vinda direto do controller");
+		mv.setViewName("FormAluno");
+		mv.addObject(aluno = new Aluno());
 		return mv;
-		
 	}
 }
